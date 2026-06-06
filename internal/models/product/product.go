2ff3938ea43b601ff44f5e3084a371fa9bd327bc
m4jorskyy <igor.suchodolski@student.put.poznan.pl>
2026-06-06 22:17:54 +0200

    product repo + product model

--- internal/models/product/product.go
+++ internal/models/product/product.go
@@ -1,1 +1,17 @@
 package product
+
+import (
+	"database/sql"
+)
+
+type Product struct {
+	ID        int64        `json:"id"`
+	Name      string       `json:"name"`
+	Material  string       `json:"material"`
+	Fineness  string       `json:"fineness"`
+	Length    float64      `json:"length"`
+	Width     float64      `json:"width"`
+	Price     float64      `json:"price"`
+	Quantity  int64        `json:"quantity"`
+	DeletedAt sql.NullTime `json:"deleted_at"`
+}
